58cbee4726cc27ac3dba479e0c8984281ab77d7e
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2025-06-10 09:18:23 +0500

    updates for the web exports

--- game_over.gd
+++ game_over.gd
@@ -3,5 +3,9 @@ extends Control
 func _ready() -> void:
 	$PlayAgain.pressed.connect(_on_play_again_pressed)
 
+func _input(event: InputEvent) -> void:
+	if visible and event.is_action_pressed("flap"):
+		_on_play_again_pressed()
+
 func _on_play_again_pressed() -> void:
 	get_tree().reload_current_scene()
